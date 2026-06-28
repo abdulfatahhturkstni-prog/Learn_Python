9fcea77dd69c099e166ee2096539eeabe318a6a3
AbdulFattah Turkistani <abdulfatah.h.turkstni@gmail.com>
2026-06-28 20:43:52 +0300

    Update lesson 1 code

--- lesson_1.py
+++ lesson_1.py
@@ -1,2 +1,9 @@
-# To print a statement
-print("Hello world")
+# check number if even or odd
+num = int(input("Enter the number : "))
+
+if num % 2 ==0 :
+    print(f"number {num} is even")
+elif num %1==0 :
+    print(f"number {num} is odd")
+else :
+    print(f"this is not a number")

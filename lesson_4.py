6fffa7a7d7aaa8de949a9b3c0e920fbd98281547
AbdulFattah Turkistani <abdulfatah.h.turkstni@gmail.com>
2026-06-30 18:57:27 +0300

    username-Validator

--- lesson_4.py
+++ lesson_4.py
@@ -1,1 +1,11 @@
-print("")
+# variables enter user input example
+username = input("Enter your username : ")
+
+if 12>len(username):
+    print("username must be more than 12 characters")
+elif not username.find(" ")==-1:
+        print("username must not contain space")    
+elif not username.isalpha():    
+    print("username must contain only numbers and alphabets")
+else:
+    print(F"username is {username}")

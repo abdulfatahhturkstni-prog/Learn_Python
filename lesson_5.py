635ccceb166dcc48821b8839b47f96ff470710ca
AbdulFattah Turkistani <abdulfatah.h.turkstni@gmail.com>
2026-07-01 14:30:17 +0300

    What food do you like

---
+++ lesson_5.py
@@ -0,0 +1,7 @@
+# lesson 5: while loop
+food = input("Enter food that you like? (q to quit): ")
+while not food == "q" :
+    print(f"you like {food}")
+    food = input("Enter another food that you like? (q to quit): ")
+print("Thanks for sharing your favorite foods!")
+

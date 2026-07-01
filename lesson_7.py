ab99ac594560b16f52cb0ac3d56e820ad0880de4
AbdulFattah Turkistani <abdulfatah.h.turkstni@gmail.com>
2026-07-01 21:24:52 +0300

    Pattern Printer

---
+++ lesson_7.py
@@ -0,0 +1,8 @@
+#Symbol Grid Generator
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter the symbol to use: ")
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end=" ")
+    print()

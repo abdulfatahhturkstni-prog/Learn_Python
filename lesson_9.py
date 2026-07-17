06b34da06f4fd7af231446eb3ef1f31d1592ae18
AbdulFattah Turkistani <abdulfatah.h.turkstni@gmail.com>
2026-07-17 18:52:35 +0300

    Python Assignment Operators

---
+++ lesson_9.py
@@ -0,0 +1,35 @@
+# Python Assignment Operators
+x = 10
+x+=5  #  x+=
+print(x) # equals 15
+print("-"*40)
+
+z = 20 
+z-=10   #z-=
+print(z) # equals 10
+print("-"*40)
+
+y = 30 
+y*=2  # y*=
+print(y)
+print("-"*40)
+
+i = 40 
+i/=2
+print(i) # equals 20.0
+print("-"*40)
+
+j = 50 
+j//=2
+print(j) # equals 25
+
+a=60
+a%=2
+print("-"*40)
+
+print(a) # equals 0
+print("-"*40)
+
+b = 5
+b**=2
+print(b) # equals 25

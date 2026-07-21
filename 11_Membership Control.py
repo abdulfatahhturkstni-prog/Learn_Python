d4456c6c642d5af2a597e3dc503b7d07f9df8e8f
AbdulFattah Turkistani <abdulfatah.h.turkstni@gmail.com>
2026-07-21 18:21:48 +0300

    Membership Control

---
+++ 11_Membership Control.py
@@ -0,0 +1,38 @@
+# ----------------------------------
+# -- Practical Membership Control --
+# ----------------------------------
+
+admins = ["Abdulfattah","Mohmmed","Abdullah","Ahmed"]
+
+name = input("please enter your Name: ").strip().capitalize()
+
+if name in admins:
+    print(f"Welcome {name} to the administration panel")
+    option = input("Please enter your option (update or delete): ").strip().lower()
+    
+    if option == "update" or option == "u":
+        new_name = input("Please enter your new name: ").strip().capitalize()
+        admins[admins.index(name)] = new_name
+        print(f"This is the new list after updating {name} to {new_name}")
+        print(admins)
+
+    elif option == "delete" or option == "d" :
+        print(f"You have deleted {name} from the administration panel")
+        admins.remove(name)
+        print(f"This is the list after deleting {name}")
+        print(admins)
+    else:
+        print("Invalid option selected.")
+else:
+   status = input("your not admin, do you want to be an admin? (y/n)").strip().lower()
+   if status == "y" or status=="Yes":
+       admins.append(name)
+       print(f"{name} you are now an admin")
+       print(admins)  
+
+   else:
+       print("You Are not Add , good bye")
+
+
+       
+
